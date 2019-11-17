fb61a8e8abdfb9a440754614c1debb35a85288bc
Martin Bagaram <martinobag@gmail.com>
2019-11-17 01:20:22 -0800

    lesson_2

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Martin Bagaram',
+        'title': 'First post',
+        'content': 'My posts content',
+        'date_posted': '11-12-2019'
+    },
+    {
+        'author': 'Bella Bagaram',
+        'title': 'First post',
+        'content': 'Bella post content',
+        'date_posted': '11-12-2019'
+    }
+]
+
 
 @app.route('/')
 @app.route('/home')
 def hello_world():
-    return 'Hello, World and friends! I did some changes'
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return 'This is about me not anayone else'
+    return render_template('about.html', title='About')
 
 
 if __name__=='__main__':
